340add27648566ae4bdaa6edfa5684b422c41fe1
EmreeDevecii <emredeveci58@gmail.com>
2024-01-11 18:02:21 +0300

    first commit

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productformmvcapp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace productformmvcapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
